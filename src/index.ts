533371b35818f1653dd2d227bb628e2d4cd8da69
Aoichaan0513 <aoichaan0513@gmail.com>
2023-02-06 02:59:45 +0900

    [Add] utils/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export * from './interfaces';
 export * from './libs';
+export * from './utils';
---
+++ src/utils/index.ts
@@ -0,0 +1,18 @@
+import {APIGuild, APIRole} from 'discord-api-types/v10';
+import {APIGuildChannel, OAuthGuild} from '../interfaces';
+
+export const getGuildIcon = (guild: OAuthGuild | APIGuild) => guild.icon ? `https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}.png` : 'https://cdn.discordapp.com/embed/avatars/0.png';
+
+export const hasPermission = (guild: OAuthGuild | APIGuild, permission: number = 0x20) => guild.owner || (Number(guild.permissions) & permission) === permission;
+
+export const sortOAuthGuilds = (guilds: OAuthGuild[]) => (guilds?.slice() ?? []).sort((a, b) => a.name.localeCompare(b.name));
+
+export const sortGuilds = (guilds: APIGuild[]) => (guilds?.slice() ?? []).sort((a, b) => a.name.localeCompare(b.name));
+
+export const sortChannels = (channels: APIGuildChannel[]) => (channels?.slice() ?? []).sort((a, b) => a.position - b.position);
+
+export const sortRoles = (roles: APIRole[]) => (roles?.slice() ?? []).sort((a, b) => b.position - a.position);
+
+export const filterPredicateChannel = (channel: APIGuildChannel, keyword: string) => keyword.length < 1 || channel.id.includes(keyword) || channel.name.toLowerCase().includes(keyword.toLowerCase());
+
+export const filterPredicateRole = (role: APIRole, keyword: string) => keyword.length < 1 || role.id.includes(keyword) || role.name.toLowerCase().includes(keyword.toLowerCase());
