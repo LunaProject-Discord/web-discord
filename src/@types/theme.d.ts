cfb2e2bfd6cb68c5bb2c76b47bfb2f1274139cb4
Aoichaan0513 <aoichaan0513@gmail.com>
2025-01-24 21:39:31 +0900

    [Update] React 19 & Next.js 15 に向けた更新

--- src/@types/theme.d.ts
+++ src/@types/theme.d.ts
@@ -2,7 +2,6 @@ import '@emotion/react';
 import { DiscordTheme as DiscordTheme } from '../styles';
 
 declare module '@emotion/react' {
-    // tslint:disable-next-line:no-empty-interface
     interface Theme extends DiscordTheme {
 
     }
